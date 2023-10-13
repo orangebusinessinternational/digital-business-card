27ca025d5de106bd87047787f57a02d27963c9a1
Mahmoud Ibrahim <mahmoud.k.ibrahim.ext@orange.com>
2023-10-13 11:25:57 +0300

    dheeraj init

---
+++ docs/cards/dheeraj/settings.js
@@ -0,0 +1,26 @@
+var name = "Dheeraj Saxena"
+var responsibility = 'Automation & Digital Business Consulting | Automation & RPA | International Consulting | Americas'
+var linkedIn = "https://www.linkedin.com/in/dheerajdigitalautomation"
+var mailto = "mailto:dheeraj.saxena@orange.com"
+var titles = [
+    'Automation & Digital Business Consulting ( Automation & RPA )',
+    'International Consulting - Americas',
+    'Orange Business',
+]
+
+var titlesDiv = document.getElementsByClassName("jobTitlesDiv")[0]
+for(var i=0; i<titles.length; i++) {
+    var label = document.createElement("label")
+    label.innerHTML = titles[i]
+    titlesDiv.appendChild(label)
+}
+
+var fullname = document.getElementById("fullName")
+var responsibiltyP = document.getElementById("responsibiltyP")
+var linkedinLink = document.getElementById("linkedinLink")
+var email = document.getElementById("locationLink")
+
+fullname.innerHTML = name
+responsibiltyP.innerHTML = responsibility
+linkedinLink.href = linkedIn
+email.href = mailto
