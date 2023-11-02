aaae3c5cb394fbbc4e2bd22f8bff80ff9ec1d1af
Mahmoud Ibrahim <mahmoud.k.ibrahim.ext@orange.com>
2023-11-02 14:10:34 +0200

    adam initial

---
+++ docs/cards/adam/settings.js
@@ -0,0 +1,27 @@
+var name = "Adam Kimmick"
+var responsibility = ''
+var linkedIn = "https://www.linkedin.com/in/adamkimmick/"
+var mailto = "mailto:adam.kimmick@orange.com"
+var titles = [
+    'Head of Sales',
+    'East, South and Central US',
+    'Orange Business',
+]
+
+
+var titlesDiv = document.getElementsByClassName("jobTitlesDiv")[0]
+for(var i=0; i<titles.length; i++) {
+    var label = document.createElement("label")
+    label.innerHTML = titles[i]
+    titlesDiv.appendChild(label)
+}
+
+var fullname = document.getElementById("fullName")
+var responsibiltyP = document.getElementById("responsibiltyP")
+var linkedinLink = document.getElementById("linkedinLink")
+var email = document.getElementById("locationLink")
+
+fullname.innerHTML = name
+responsibiltyP.innerHTML = responsibility
+linkedinLink.href = linkedIn
+email.href = mailto
