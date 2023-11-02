f4ffa165a70d5b128d5102fbdf671101ae7a0270
Mahmoud Ibrahim <mahmoud.k.ibrahim.ext@orange.com>
2023-11-02 13:55:51 +0200

    paulo orio - initial

---
+++ docs/cards/paulo/settings.js
@@ -0,0 +1,27 @@
+var name = "Paulo Orio"
+var responsibility = ''
+var linkedIn = "https://www.linkedin.com/in/pauloorio/"
+var mailto = "mailto:paulo.orio@orange.com"
+var titles = [
+    'Director of Sales & Pre-Sales',
+    'Latin America',
+    'Orange Business',
+]
+
+
+var titlesDiv = document.getElementsByClassName("jobTitlesDiv")[0]
+for(var i=0; i<titles.length; i++) {
+    var label = document.createElement("label")
+    label.innerHTML = titles[i]
+    titlesDiv.appendChild(label)
+}
+
+var fullname = document.getElementById("fullName")
+var responsibiltyP = document.getElementById("responsibiltyP")
+var linkedinLink = document.getElementById("linkedinLink")
+var email = document.getElementById("locationLink")
+
+fullname.innerHTML = name
+responsibiltyP.innerHTML = responsibility
+linkedinLink.href = linkedIn
+email.href = mailto
