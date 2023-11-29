3acfb9d7e785649213afc68d2cf3f84ebbeaf590
Aly Shaheen <aly.shaheen@orange.com>
2023-11-29 12:29:19 +0200

    added hriday

---
+++ docs/card.py
@@ -0,0 +1,31 @@
+import os
+
+import qrcode
+from qrcode.image import svg
+from qrcode.image.styledpil import StyledPilImage
+from qrcode.image.styles.moduledrawers import RoundedModuleDrawer
+
+
+LOGO_IMAGE_FILE = r'C:\Users\HGWC0974\Desktop\Projects\digital-business-card\docs\static\images\orange_logo.png'
+
+
+def generate_qrcode(url, filename, logo=LOGO_IMAGE_FILE):
+	qr = qrcode.QRCode(
+		version = 1,
+		error_correction = qrcode.constants.ERROR_CORRECT_H,
+		box_size=10,
+		border=4
+	)
+
+	qr.add_data(url)
+	qr.make(fit=True)
+
+	img = qr.make_image(image_factory=StyledPilImage, module_drawer=RoundedModuleDrawer(), embeded_image_path=logo)
+	img.save(filename)
+
+
+if __name__ == '__main__':
+	url = 'https://orangebusinessinternational.github.io/digital-business-card/cards/hriday/light.pdf'
+	filename = r'C:\Users\HGWC0974\Desktop\Projects data\business card\qr.jpg'
+
+	generate_qrcode(url, filename)
