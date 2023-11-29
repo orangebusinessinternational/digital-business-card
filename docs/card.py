845960c19a6219847d162cd64e70937096454a38
Aly Shaheen <aly.shaheen@orange.com>
2023-11-29 13:14:34 +0200

    aliette

--- docs/card.py
+++ docs/card.py
@@ -25,7 +25,7 @@ def generate_qrcode(url, filename, logo=LOGO_IMAGE_FILE):
 
 
 if __name__ == '__main__':
-	url = 'https://orangebusinessinternational.github.io/digital-business-card/cards/wassila/light.pdf'
+	url = 'https://orangebusinessinternational.github.io/digital-business-card/cards/aliette/light.pdf'
 	filename = r'C:\Users\HGWC0974\Desktop\Projects data\business card\qr.jpg'
 
 	generate_qrcode(url, filename)
